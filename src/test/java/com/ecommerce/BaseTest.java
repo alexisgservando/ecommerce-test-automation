3e401c790e153e73817300925ca2a6a8230c9eec
Alexis Guardado <alexisgs007@hotmail.com>
2025-08-06 12:14:04 -0600

    Fix: add headless chrome config for CI

--- src/test/java/com/ecommerce/BaseTest.java
+++ src/test/java/com/ecommerce/BaseTest.java
@@ -9,27 +9,31 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class BaseTest {
-	protected WebDriver driver;
+    protected WebDriver driver;
 
-	@BeforeClass
-	public void setUp() {
-		WebDriverManager.chromedriver().setup();
-		
-		// Set Chrome options for headless mode
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--headless=new"); // Use 'new' for Chrome 109+
-		options.addArguments("--no-sandbox");
-		options.addArguments("--disable-dev-shm-usage");
-		
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get(ConfigReader.getBaseURL());
-	}
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
 
-	@AfterClass
-	public void tearDown() {
-		if (driver != null) {
-			driver.quit();
-		}
-	}
+        ChromeOptions options = new ChromeOptions();
+
+        // Always run headless in CI
+        if (System.getenv("CI") != null) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--remote-allow-origins=*");
+        }
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get(ConfigReader.getBaseURL());
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
