03cc1d07d426f7d781afef266c1e743046d21cd3
Alexis Guardado <alexisgs007@hotmail.com>
2025-08-12 09:41:17 -0600

    Updates to Basetest.Java class

--- src/test/java/com/ecommerce/BaseTest.java
+++ src/test/java/com/ecommerce/BaseTest.java
@@ -1,24 +1,19 @@
 package com.ecommerce;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import utils.ConfigReader;
-
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.Map;
-import java.util.UUID;
-
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import utils.ConfigReader;
 
 public class BaseTest {
 	protected WebDriver driver;
